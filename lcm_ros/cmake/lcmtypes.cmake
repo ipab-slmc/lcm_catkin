688cbf8de31e696931c2988046aba79f5aa86a6a
Vladimir Ivan <v.ivan@ed.ac.uk>
2016-11-10 18:25:12 +0000

    Updated lcm_ros package - Java compiles correctly (including lcm-spy plugins) - Both devel and install space have been fixed - Added dependency on LCM via oh-distro

--- lcm_ros/cmake/lcmtypes.cmake
+++ lcm_ros/cmake/lcmtypes.cmake
@@ -11,17 +11,25 @@ set(_lcmtypes_hpp_files_in)
 set(_lcmtypes_hpp_files)
 set(_lcmtypes_py_files)
 set(_lcmtypes_java_files)
+set(_lcm_find_path)
+set(_spy_lcm_jars)
+set(JAVA_INSTALL_JARS)
+set(LCMTYPES_JAR ${CATKIN_DEVEL_PREFIX}/share/java/lcmtypes_${PROJECT_NAME}.jar)
 set(libname "lcmtypes_${PROJECT_NAME}")
-set(_lcm_redirct_script ${CMAKE_CURRENT_LIST_DIR}/redirect.py)
+set(_lcm_redirct_script python ${CMAKE_CURRENT_LIST_DIR}/redirect.py)
 add_custom_target(lcmtypes_${PROJECT_NAME}_all ALL)
 list(APPEND ${PROJECT_NAME}_EXPORTED_TARGETS "lcmtypes_${PROJECT_NAME}_all")
+
+macro(FindLCM)
+  set(_lcm_find_path ${ARGN})
+endmacro(FindLCM)
 
 macro(AddLCM)
   set(_lcmtypes_python_dir ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_PYTHON_DESTINATION})
 
   GetLCMPackageName(_lcm_package_name ${PROJECT_NAME})
   foreach(arg ${ARGN})
-    list(APPEND _lcmtypes ${CMAKE_CURRENT_SOURCE_DIR}/lcmtypes/${arg}.lcm)
+    list(APPEND _lcmtypes ${CMAKE_CURRENT_SOURCE_DIR}/${_lcm_find_path}/lcmtypes/${arg}.lcm)
     list(APPEND _lcmtypes_c_files ${_lcmtypes_c_dir}/${arg}.c)
     list(APPEND _lcmtypes_h_files ${_lcmtypes_h_dir}/${arg}.h)
     string(REPLACE "${_lcm_package_name}_" "" tmp_hpp ${arg})
@@ -32,7 +40,14 @@ macro(AddLCM)
     list(APPEND _lcmtypes_java_files ${_lcmtypes_java_dir}/${_lcm_package_name}/${tmp_hpp}.java)
     message(STATUS "Adding LCM type '${arg}'")
   endforeach()
+  list(APPEND JAVA_INSTALL_JARS ${LCMTYPES_JAR})
 endmacro(AddLCM)
+
+macro(AddSpyLCM _jarname)
+  list(APPEND _spy_lcm_jars ${_jarname})
+  list(APPEND JAVA_INSTALL_JARS ${CATKIN_DEVEL_PREFIX}/share/java/lcmspy_plugins_${_jarname}.jar)
+  set(_jar_sources_${_jarname} "${ARGN}")
+endmacro(AddSpyLCM)
 
 macro(GetLCMPackageName ret name)
   if(${name} MATCHES "_lcmtypes")
@@ -162,7 +177,7 @@ function(lcmtypes_build_python)
       TARGET ${PROJECT_NAME}_lcmgen_py
       COMMAND sh -c '([ -d ${_lcmtypes_python_dir_in} ] || mkdir -p ${_lcmtypes_python_dir_in}) && ${LCM_GEN_EXECUTABLE} -p ${_lcmtypes} --ppath ${_lcmtypes_python_dir_in}'
       COMMAND sh -c '([ -d ${_lcmtypes_python_dir} ] || mkdir -p ${_lcmtypes_python_dir}) && cp ${_lcmtypes_python_dir_in}/${_lcm_package_name}/* ${_lcmtypes_python_dir}'
-      COMMAND sh -c '${_lcm_redirct_script} ${_num_lcmtypes} ${_lcmtypes_py_files} ${_lcm_packages}'
+      COMMAND sh -c '${_lcm_redirct_script} ${_num_lcmtypes} ${_lcmtypes_py_files} ${_lcm_packages} && python -m compileall ${_lcmtypes_python_dir}'
       DEPENDS ${_lcmtypes}
     )
 
@@ -185,12 +200,13 @@ function(lcmtypes_build_java)
         return()
     endif()
     string(STRIP ${LCM_JAR_FILE} LCM_JAR_FILE)
-    set(LCMTYPES_JAR ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_LIB_DESTINATION}/lcmtypes_${PROJECT_NAME}.jar)
+    
 
 
     add_custom_command(
       OUTPUT ${_lcmtypes_java_files}
       COMMAND sh -c '([ -d ${_lcmtypes_java_dir} ] || mkdir -p ${_lcmtypes_java_dir}) && ${LCM_GEN_EXECUTABLE} -j ${_lcmtypes} --jpath ${_lcmtypes_java_dir}'
+      COMMAND sh -c '([ -d ${CATKIN_DEVEL_PREFIX}/share/java ] || mkdir -p ${CATKIN_DEVEL_PREFIX}/share/java) '
       DEPENDS ${_lcmtypes}
       COMMENT "Generating LCM types (Java)"
     )
@@ -201,13 +217,20 @@ function(lcmtypes_build_java)
 
     # search for lcmtypes_*.jar files in well-known places and add them to the
     # classpath
+    string(REPLACE ":" ";" ROS_PACKAGE_PATH $ENV{ROS_PACKAGE_PATH})
     set(_java_deps)
     foreach(pfx ${catkin_ALL_FOUND_COMPONENTS})
         if(";${${pfx}_EXPORTED_TARGETS};" MATCHES ";lcmtypes_${pfx}_all;")
-            set(java_classpath ${java_classpath}:${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_LIB_DESTINATION}/lcmtypes_${pfx}.jar)
+            set(java_classpath ${java_classpath}:${CATKIN_DEVEL_PREFIX}/share/java/lcmtypes_${pfx}.jar:${CMAKE_INSTALL_PREFIX}/share/java/lcmtypes_${pfx}.jar)
+            foreach(pth ${ROS_PACKAGE_PATH})
+                set(java_classpath ${java_classpath}:${pth}/../share/java/lcmtypes_${pfx}.jar:${pth}/../share/java/lcmtypes_${pfx}.jar)
+            endforeach()
+            set(java_classpath ${java_classpath}:/opt/oh-distro/share/java/lcmtypes_${pfx}.jar)
             list_append_unique(_java_deps lcmtypes_${pfx}_all)
         endif()
     endforeach()
+
+    #message(Java: ${java_classpath})
 
     # convert the list of .java filenames to a list of .class filenames
     foreach(javafile ${_lcmtypes_java_files})
@@ -216,11 +239,9 @@ function(lcmtypes_build_java)
         unset(__tmp_class_fname)
     endforeach()
 
-    MESSAGE(" Java: " ${java_classpath})
-
     # add a rule to build the .class files from from the .java files
     add_custom_command(OUTPUT ${_lcmtypes_class_files}
-        COMMAND ${JAVA_COMPILE} -source 6 -target 6 -cp ${java_classpath} ${_lcmtypes_java_files}
+        COMMAND ${JAVA_COMPILE} -source 7 -target 7 -cp ${java_classpath} ${_lcmtypes_java_files}
         DEPENDS ${_lcmtypes_java_files}
         VERBATIM
         COMMENT "Compiling LCM types (Java)"
@@ -234,7 +255,91 @@ function(lcmtypes_build_java)
     add_custom_target(lcmtypes_${PROJECT_NAME}_jar ALL DEPENDS ${LCMTYPES_JAR})
 
     add_dependencies(lcmtypes_${PROJECT_NAME}_jar ${PROJECT_NAME}_lcmgen_java ${_java_deps} ${catkin_EXPORTED_TARGETS})
-    add_dependencies(lcmtypes_${PROJECT_NAME}_all lcmtypes_${PROJECT_NAME}_jar)
+    add_dependencies(lcmtypes_${PROJECT_NAME}_all lcmtypes_${PROJECT_NAME}_jar)    
+
+endfunction()
+
+function(lcmtypes_build_java_plugins)
+
+    list(LENGTH _spy_lcm_jars _num_plugins)
+    if(_num_plugins EQUAL 0)
+        return()
+    endif()
+
+    find_package(Java)
+
+    
+
+    # do we have LCM java bindings?  where is lcm.jar?
+    execute_process(COMMAND pkg-config --variable=classpath lcm-java OUTPUT_VARIABLE LCM_JAR_FILE)
+    if(NOT LCM_JAR_FILE)
+        message(STATUS "Not building Java LCM type bindings (Can't find lcm.jar)")
+        return()
+    endif()
+    string(STRIP ${LCM_JAR_FILE} LCM_JAR_FILE)
+    
+
+    set(java_classpath ${_lcmtypes_java_dir}:${LCM_JAR_FILE})
+
+    # search for lcmtypes_*.jar files in well-known places and add them to the
+    # classpath
+    string(REPLACE ":" ";" ROS_PACKAGE_PATH $ENV{ROS_PACKAGE_PATH})
+    set(_java_deps)
+    foreach(pfx ${catkin_ALL_FOUND_COMPONENTS})
+        if(";${${pfx}_EXPORTED_TARGETS};" MATCHES ";lcmtypes_${pfx}_all;")
+            set(java_classpath ${java_classpath}:${CATKIN_DEVEL_PREFIX}/share/java/lcmtypes_${pfx}.jar:${CMAKE_INSTALL_PREFIX}/share/java/lcmtypes_${pfx}.jar)
+            foreach(pth ${ROS_PACKAGE_PATH})
+                set(java_classpath ${java_classpath}:${pth}/../share/java/lcmtypes_${pfx}.jar:${pth}/../share/java/lcmtypes_${pfx}.jar)
+            endforeach()
+            set(java_classpath ${java_classpath}:/opt/oh-distro/share/java/lcmtypes_${pfx}.jar)
+            list_append_unique(_java_deps lcmtypes_${pfx}_all)
+        endif()
+    endforeach()
+    set(java_classpath ${java_classpath}:${LCMTYPES_JAR})
+
+    foreach(_spy_jar ${_spy_lcm_jars})
+            set(LCMSPY_JAR ${CATKIN_DEVEL_PREFIX}/share/java/lcmspy_plugins_${_spy_jar}.jar)
+
+	    #message(Java: ${java_classpath})
+
+            set(_spy_java_files_in ${_jar_sources_${_spy_jar}})
+            set(_spy_java_files)
+            #message(JAR: ${_spy_jar})
+            #message(src: ${_spy_java_files_in})
+
+	    # convert the list of .java filenames to a list of .class filenames
+	    foreach(javafile ${_spy_java_files_in})
+		get_filename_component(__tmp_class_fname ${javafile} NAME_WE)
+		list(APPEND _spy_class_files ${_lcmtypes_java_dir}/spy_plugins/${__tmp_class_fname}.class)
+                get_filename_component(__tmp_java_fname ${javafile} ABSOLUTE)
+                list(APPEND _spy_java_files ${__tmp_java_fname})
+		unset(__tmp_class_fname)
+                unset(__tmp_java_fname)
+	    endforeach()
+
+            #message(src: ${_spy_java_files})
+	    #message(calss: ${_spy_class_files})
+
+	    # add a rule to build the .class files from from the .java files
+	    add_custom_command(OUTPUT ${_spy_class_files}
+		COMMAND sh -c "([ -d ${_lcmtypes_java_dir}/spy_plugins ] || mkdir -p ${_lcmtypes_java_dir}/spy_plugins) "
+		COMMAND sh -c "([ -d ${CATKIN_DEVEL_PREFIX}/share/java ] || mkdir -p ${CATKIN_DEVEL_PREFIX}/share/java) "
+		COMMAND ${JAVA_COMPILE} -source 7 -target 7 -cp ${java_classpath} -d ${_lcmtypes_java_dir}/spy_plugins ${_spy_java_files}
+		DEPENDS ${_spy_java_files}
+		VERBATIM
+		COMMENT "Compiling LCM-SPY plugins (Java)"
+	    )
+
+	    # add a rule to build a .jar file from the .class files
+	    add_custom_command(OUTPUT ${LCMSPY_JAR}
+		COMMAND ${JAVA_ARCHIVE} cf ${LCMSPY_JAR} -C ${_lcmtypes_java_dir}/spy_plugins .
+		DEPENDS ${_spy_class_files}
+		VERBATIM)
+	    add_custom_target(lcmspy_${_spy_jar}_jar ALL DEPENDS ${LCMSPY_JAR})
+
+	    add_dependencies(lcmspy_${_spy_jar}_jar lcmtypes_${PROJECT_NAME}_jar ${_java_deps} ${catkin_EXPORTED_TARGETS})
+	    add_dependencies(lcmtypes_${PROJECT_NAME}_all lcmspy_${_spy_jar}_jar)
+    endforeach()
 
 endfunction()
 
@@ -249,6 +354,20 @@ macro(GenerateLCM)
     lcmtypes_build_c()
     lcmtypes_build_cpp()
     lcmtypes_build_java(${ARGV})
+    lcmtypes_build_java_plugins()
     lcmtypes_build_python()
+
+    install(TARGETS ${libname}
+        ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+        LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
+        RUNTIME DESTINATION ${CATKIN_GLOBAL_BIN_DESTINATION})
+    install(DIRECTORY ${_lcmtypes_h_dir}/
+        DESTINATION ${CATKIN_PACKAGE_INCLUDE_DESTINATION}
+        PATTERN ".svn" EXCLUDE)
+    install(DIRECTORY ${_lcmtypes_python_dir}/
+        DESTINATION ${CATKIN_PACKAGE_PYTHON_DESTINATION}
+        PATTERN ".svn" EXCLUDE)
+    install(FILES ${JAVA_INSTALL_JARS}
+        DESTINATION share/java)
     #lcmtypes_install_types()
 endmacro()
