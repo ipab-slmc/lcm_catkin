5b46645ae559dfc44ce3902f7c30bcb42735da7d
Christian Rauch <Christian.Rauch@ed.ac.uk>
2017-02-03 10:58:03 +0000

    Update README and scripts
    - debian source list in dedicated file
    - fix pipe with sudo
    - manual installation of packages with "--force-yes"

--- rosdep/update-rosdep.bash
+++ rosdep/update-rosdep.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo yaml http://terminator.robots.inf.ed.ac.uk/apt/rosdep.yaml > sudo /etc/ros/rosdep/sources.list.d/30-oh.list
+sudo sh -c 'echo "yaml http://terminator.robots.inf.ed.ac.uk/apt/rosdep.yaml" > /etc/ros/rosdep/sources.list.d/30-oh.list'
